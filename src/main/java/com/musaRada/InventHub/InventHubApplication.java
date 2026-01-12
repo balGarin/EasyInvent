6908f276b6f1b263dcd10a458cfe36f73d89ea68
gabaNet66 <rin9a@yandex.ru>
2026-01-12 14:51:42 +0300

    Initial commit

---
+++ src/main/java/com/musaRada/InventHub/InventHubApplication.java
@@ -0,0 +1,13 @@
+package com.musaRada.InventHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/musaRada/InventHub/InventHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musaRada.InventHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
